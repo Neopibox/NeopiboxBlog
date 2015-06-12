35aff9ef08c4fac1d6b7dc16518fd74ac3f62d6c
Neopibox <roue.evan1@gmail.com>
2015-06-12 16:34:47 +0200

    New progress bar style
    Enhanced progress bar style for the admin dashboard.

--- public/js/admin/dashboard/refresh.js
+++ public/js/admin/dashboard/refresh.js
@@ -9,17 +9,12 @@ function callback()
 	{
 		$.getJSON('admin/dashboard/refresh', function(data)
 		{
-			/*CPU_usage = i;
-			RAM_usage = i;
-			SWAP_usage = i;
-			HDD_usage = i;*/
-
-			$('#CPU > span').attr('style', 'width: ' + data.CPU_usage + '%;');
-			$('#RAM > span').attr('style', 'width: ' + data.RAM_usage + '%;');
-			$('#SWAP > span').attr('style', 'width: ' + data.SWAP_usage + '%;');
-			$('#HDD > span').attr('style', 'width: ' + data.HDD_usage + '%;');
+			$('#CPU > span').attr('style', 'width: ' + (100 - data.CPU_usage) + '%;');
+			$('#RAM > span').attr('style', 'width: ' + (100 - data.RAM_usage) + '%;');
+			$('#SWAP > span').attr('style', 'width: ' + (100 - data.SWAP_usage) + '%;');
+			$('#HDD > span').attr('style', 'width: ' + (100 - data.HDD_usage) + '%;');
 		});
 	}
 }
 
-var truc = setInterval(callback, 500);
+var truc = setInterval(callback, 1000);
